22f51b198854611b4fcfccddef30feeebfbce117
TanLui <effishing@gmail.com>
2024-05-28 13:49:49 +0200

    connected to openai

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-import 'dotenv/config'
-
+import dotenv from 'dotenv'
 import './config/.env'
+import OpenAI from 'openai'
 
-const secretKey = process.env.OPENAI_KEY
-console.log(process.env.OPENAI_KEY)
+dotenv.config()
+
+// const secretKey = process.env.OPENAI_KEY
+
+// const configuration = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// })
+
+const openAi = new OpenAI()
+
+async function main() {
+  // const chat2 = OpenAI(configuration)
+  const chatComplete = await openAi.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: 'what is capital of china?',
+      },
+    ],
+  })
+  console.log(chatComplete.choices[0].message)
+}
+
+main()
